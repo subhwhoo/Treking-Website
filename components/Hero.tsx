330adf715040dceb0b84fa5750ed2b5e10ffaacf
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-11-10 19:26:54 +0530

    Complete Website

---
+++ components/Hero.tsx
@@ -0,0 +1,85 @@
+import Image from 'next/image'
+import Button from './Button'
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image 
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+        />
+        <h1 className="bold-52 lg:bold-88">Kheerganga Trek, Kasol</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+        Kheerganga is one of the unique Himalayan treks in which you are rewarded with excellent meadows, forested ways with pine trees, couple of cascades and crossing towns on reaching the summit at almost 10,000 feet
+        </p>
+
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5).fill(1).map((_, index) => (
+              <Image 
+                src="/star.svg"
+                key={index}
+                alt="star"
+                width={24}
+                height={24}
+              />
+            ))}
+          </div>
+
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            1.5K
+            <span className="regular-16 lg:regular-20 ml-1">Positive Reviews</span>
+          </p>
+        </div>
+
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button 
+            type="button" 
+            title="Download App" 
+            variant="btn_green" 
+          />
+          <Button 
+            type="button" 
+            title="How we work?" 
+            icon="/play.svg"
+            variant="btn_white_text" 
+          />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+
+           <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Himachal Pradesh</p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">12 km </p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">9,700 feet</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
+
+
